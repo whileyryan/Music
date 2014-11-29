7e89ef268c5792e2c668287709c775e2670193f6
Ryan Whiley <whileyryan@gmail.com>
2014-11-29 11:46:16 -0800

    geocode

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ActiveRecord::Base
+  devise :omniauthable, :omniauth_providers => [:facebook]
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.image = auth.info.image
+    end
+  end
+
+
+  def self.new_with_session(params, session)
+    #tapping into super and putting in some stuff
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+end
