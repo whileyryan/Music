853e359f24a9195aeaa2389e4d7fb07b0ec3c540
sszeto71 <stanley.szeto@sbcglobal.net>
2014-11-30 08:39:24 -0800

    Added genres and genre associations to band when artist profile page loads

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,14 +6,27 @@ class ArtistsController < ApplicationController
 
     @artist = Artist.find(params[:id])
     @reviews = @artist.reviews
-    puts @artist.inspect
     @mbid = @artist.mbid
-    puts @mbid
 
     # @artist_search = Rockstar::Artist.new('Ellie Goulding', :include_info => true)
     @artist_search = Rockstar::Artist.new(@artist.name, :mbid => @mbid, :include_info => true)
     puts @artist_search.url
     puts @artist_search.inspect
 
+    # Add genre into genre table and artist_genres tables
+    @genres = @artist_search.tags
+    if @genres
+        @genres.each do |genre|
+            if Genre.exists?(name: genre.to_s)
+                current_genre = Genre.find_by(name: genre.to_s)
+                @artist.genres << current_genre
+            else
+                new_genre = Genre.new(name: genre.to_s)
+                if new_genre.save
+                    @artist.genres << new_genre
+                end
+            end
+        end
+    end
   end
 end
