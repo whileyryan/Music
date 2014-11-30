c262c18929a5d1677df19a99837ddb441edb97bd
sszeto71 <stanley.szeto@sbcglobal.net>
2014-11-30 15:17:40 -0800

    Sorted search based on artists with reviews

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,12 +7,32 @@ class SearchController < ApplicationController
   end
 
   def show
+    # Search artists in application with reviews
+    @artists_with_reviews = []
+    search_uppercase = params[:search_query].capitalize
+    existing_artists = Artist.where("name LIKE ?", "%#{search_uppercase}%")
+
+    if existing_artists.count > 0
+      existing_artists.each do |artist|
+        if artist.reviews.count > 0
+          @artists_with_reviews.push(artist)
+        end
+      end
+    end
+
+    puts "**** Artists with reviews"
+    puts @artists_with_reviews.inspect
+
+    # Find remaining artists via Last.fm
     Dotenv.load
     lastfm = Lastfm.new(ENV['LASTFM_API_KEY'], ENV['LASTFM_SECRET_KEY']);
     response = lastfm.artist.search(artist: params[:search_query], limit:10);
     # response = lastfm.artist.getInfo({artist: params[:search_query]}, {api_key: ENV['LASTFM_API_KEY']});
     @results = response['results']['artistmatches']
     @ruby_artists = create_artists(@results)
+
+    # combined results removing duplicate artists
+    @prioritize_artists = @artists_with_reviews | @ruby_artists
 
   end
 
