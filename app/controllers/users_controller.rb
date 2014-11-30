d9c5bda567b8348b43cbcd0bab182237911710b3
arianakvergara <ariana.vergara@gmail.com>
2014-11-30 13:51:12 -0800

    dropdown menu for profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ require_dependency "concert"
 class UsersController < ApplicationController
   before_action :authenticate_user!
   skip_before_filter  :verify_authenticity_token
-  
+
 
   def show
-  	# @concerts = Event.all
+  	@concerts = Event.all
   	# for later
-  	zipcode = current_user.zipcode
-  	@concerts = Concert.get_concerts(zipcode)
+  	# zipcode = current_user.zipcode
+  	# @concerts = Concert.get_concerts(zipcode)
   end
 
   def store_zipcode
@@ -17,6 +17,6 @@ class UsersController < ApplicationController
   	@user = current_user
   	@user.update_attributes(:zipcode => params[:zip])
   	@user.save
-  	p @user     
+  	p @user
   end
 end
