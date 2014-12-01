4665b5699426a2de059b14d997f65e3ec0a6d626
sszeto71 <stanley.szeto@sbcglobal.net>
2014-11-30 16:10:47 -0800

    Initial avg artist ratings calculations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,6 +12,41 @@ class ReviewsController < ApplicationController
         format.js {render 'review'}
       end
     end
+
+    # calculate and update artist scoring
+    @artist = Artist.find(params[:artist_id])
+    @reviews = @artist.reviews
+    # initialize rating
+    @music_rating = 0
+    @atmosphere_rating = 0
+    @overall_rating = 0
+    @composite_rating = 0
+
+    # @reviews.each do |review|
+    #   @music_rating += review.music_rating
+    #   @atmosphere_rating += review.atmosphere_rating
+    #   @overall_rating += review.overall_rating
+    #   @composite_rating += review.composite_rating
+    # end
+
+    puts "**** Artist Ratings *****"
+    puts @music_rating
+    puts @atmosphere_rating
+    puts @overall_rating
+    puts @composite_rating
+
+    # simple average
+    @avg_music = @music_rating/@reviews.count
+    @avg_atmosphere = @atmosphere_rating/@reviews.count
+    @avg_overall = @overall_rating/@reviews.count
+    @avg_composite = @composite_rating/@reviews.count
+
+    # update artist's ratings
+    @artist.music_rating = @avg_music
+    @artist.atmosphere_rating = @avg_atmosphere
+    @artist.overall_rating = @avg_overall
+    @artist.composite_rating = @avg_composite
+    @artist.save
   end
 
   private
