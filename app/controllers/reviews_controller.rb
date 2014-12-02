a9f1522878deb25a5b113aa631bad50c4d692636
sszeto71 <stanley.szeto@sbcglobal.net>
2014-12-01 19:21:12 -0800

    Updated and refactor reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,38 +5,40 @@ class ReviewsController < ApplicationController
   def create
 
     @review = Review.new(review_params)
-    @review.save
-    @artist = Artist.find(params[:artist_id])
-    # calculate and update artist scoring
-    @reviews = @artist.reviews
-    # initialize rating
-    @music_rating = 0
-    @atmosphere_rating = 0
-    @overall_rating = 0
-    @composite_rating = 0
-
-    @reviews.each do |review|
-      @music_rating += review.music_rating.to_i
-      @atmosphere_rating += review.atmosphere_rating.to_i
-      @overall_rating += review.overall_rating.to_i
-      @composite_rating += review.composite_rating.to_i
-    end
-
-    # simple average without weighting
-    @avg_music = @music_rating/@reviews.count
-    @avg_atmosphere = @atmosphere_rating/@reviews.count
-    @avg_overall = @overall_rating/@reviews.count
-    @avg_composite = @composite_rating/@reviews.count
-
-    # update artist's ratings
-    @artist.music_rating = @avg_music
-    @artist.atmosphere_rating = @avg_atmosphere
-    @artist.overall_rating = @avg_overall
-    @artist.composite_rating = @avg_composite
-    @artist.save
 
     respond_to do |format|
-      format.js {render 'review'}
+      if @review.save
+        @artist = Artist.find(params[:artist_id])
+        # calculate and update artist scoring
+        @reviews = @artist.reviews
+        # initialize rating
+        @music_rating = 0
+        @atmosphere_rating = 0
+        @overall_rating = 0
+        @composite_rating = 0
+
+        @reviews.each do |review|
+          @music_rating += review.music_rating.to_i
+          @atmosphere_rating += review.atmosphere_rating.to_i
+          @overall_rating += review.overall_rating.to_i
+          @composite_rating += review.composite_rating.to_i
+        end
+
+        # simple average without weighting
+        @avg_music = @music_rating/@reviews.count
+        @avg_atmosphere = @atmosphere_rating/@reviews.count
+        @avg_overall = @overall_rating/@reviews.count
+        @avg_composite = @composite_rating/@reviews.count
+
+        # update artist's ratings
+        @artist.music_rating = @avg_music
+        @artist.atmosphere_rating = @avg_atmosphere
+        @artist.overall_rating = @avg_overall
+        @artist.composite_rating = @avg_composite
+        @artist.save
+
+        format.js {render 'review'}
+      end
     end
 
   end
