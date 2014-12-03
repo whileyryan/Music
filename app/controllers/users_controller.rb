13d2f2b9090222a24290e91395810c8fda93bdd2
Ryan Whiley <whileyryan@gmail.com>
2014-12-02 16:07:23 -0800

    got infinite scroll working w/out zip search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,17 +18,39 @@ class UsersController < ApplicationController
     end
   end
 
+  def original_concerts
+    concerts_all = Concert.storeConcerts(Event.all)
+    if request.xhr?
+        render json: concerts_all.to_json
+    end
+  end
+
+  def load_concerts
+    
+    i = params['id'].to_i
+    concerts = Event.where(:id => (i+1 .. i+11))   
+    p '='*100
+    p concerts
+    p '='*100
+    if request.xhr?
+        render json: concerts.to_json
+    end
+    # respond_to do |format|
+    #   format.json { @concerts.to_json }
+    # end
+  end
+   
   def show
     @reviews = Review.all
     @user = current_user
     url = "/users/#{@user.id}"
     if !params.include?('current_location')
       @zipcode = current_user.zipcode
-      @concerts = Event.all
+      # @concerts = Concert.storeConcerts(Event.all)
     # @concerts = Concert.get_concerts(@zipcode)
     else
       @zipcode = params['current_location']
-      @concerts = Event.all
+      # @concerts = Concert.storeConcerts(Event.all)
       # @concerts = Concert.get_concerts(@zipcode)
     end
     # for later
@@ -36,9 +58,11 @@ class UsersController < ApplicationController
 
   def store_zipcode
     @user = current_user
+    p params
     @user.update_attributes(:zipcode => params[:zip])
     @user.save
     render nothing: true
   end
 
 end
+   
