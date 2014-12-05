5b53665dc02517662f7142f89a5f182b89afa46b
Stan Szeto <stanley.szeto@sbcglobal.net>
2014-12-04 18:27:28 -0800

    updated artists.js file

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -1,18 +1,31 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
 
+//   $('#toggleyoutube').on('click', function(){
+//     $('a.youtube').YouTubeModal({autoplay: true});
+//   })
+
+
+// $('#autocomplete').on('focus', function(){
+//     $('#autocomplete').geocomplete()
+//     })
+
+
+
+
+// })
+
+var ready;
+ready = function() {
   $('#toggleyoutube').on('click', function(){
     $('a.youtube').YouTubeModal({autoplay: true});
   })
 
 
-$('#autocomplete').on('focus', function(){
-    $('#autocomplete').geocomplete()
-    })
+  $('#autocomplete').on('focus', function(){
+      $('#autocomplete').geocomplete()
+      })
+};
 
-
-
-
-})
-
-
+$(document).ready(ready);
+$(document).on('page:load', ready);
